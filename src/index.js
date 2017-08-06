a2216766fb89c6a4f33f68a1ae49be0a8a345991
zhongxx <358613045@qq.com>
2017-08-06 22:52:53 +0800

    测试组件签入

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux'
-import {Router, Route, IndexRoute, hashHistory} from 'react-router';
+import { Provider } from 'react-redux'
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import './utils/index.js';  // 引入各种prototype辅助方法
 import store from 'redux/store.js';  // redux store
 
@@ -14,6 +14,9 @@ import App from './components/App';
 import Welcome from './components/Welcome';
 import Error from './components/Error';
 import Hello from './components/Hello';
+import BusiHello from './components/Busi/Hello';
+
+
 //import DBTable from './components/DBTable';
 
 // 将DBTable组件做成动态路由, 减小bundle size
@@ -31,54 +34,51 @@ const routes = (
   <Provider store={store}>
     <Router history={hashHistory}>
       <Route path="/" component={App}>
-        <IndexRoute component={Welcome}/>
-
+        <IndexRoute component={Welcome} />
         <Route path="index">
-          <Route path="option1" tableName="test" getComponent={DBTableContainer}/>
-          <Route path="option2" tableName="testSms" getComponent={DBTableContainer}/>
-          <Route path="option3" tableName="testAction" getComponent={DBTableContainer}/>
+          <Route path="option1" tableName="test" getComponent={DBTableContainer} />
+          <Route path="option2" tableName="testSms" getComponent={DBTableContainer} />
+          <Route path="option3" tableName="testAction" getComponent={DBTableContainer} />
         </Route>
-
         <Route path="daohang">
-          <Route path="555" component={Hello}/>
+          <Route path="555" component={BusiHello} />
           <Route path="sanji">
-            <Route path="666" component={Hello}/>
-            <Route path="777" component={Hello}/>
-            <Route path="888" component={Hello}/>
-            <Route path="999" component={Hello}/>
+            <Route path="666" component={Hello} />
+            <Route path="777" component={Hello} />
+            <Route path="888" component={Hello} />
+            <Route path="999" component={Hello} />
           </Route>
         </Route>
 
         <Route path="test">
-          <Route path="aaa" component={Hello}/>
-          <Route path="bbb" component={Hello}/>
-          <Route path="ccc" component={Hello}/>
+          <Route path="aaa" component={Hello} />
+          <Route path="bbb" component={Hello} />
+          <Route path="ccc" component={Hello} />
           <Route path="sanjiaaa">
-            <Route path="666aa" component={Hello}/>
+            <Route path="666aa" component={Hello} />
           </Route>
           <Route path="sanjibbb">
-            <Route path="666bb" component={Hello}/>
+            <Route path="666bb" component={Hello} />
           </Route>
         </Route>
 
         <Route path="headerMenu5">
-          <Route path="headerMenu5000000" component={Hello}/>
+          <Route path="headerMenu5000000" component={Hello} />
           <Route path="headerMenu51111">
-            <Route path="headerMenu51111aa" component={Hello}/>
-            <Route path="headerMenu51111bb" component={Hello}/>
+            <Route path="headerMenu51111aa" component={Hello} />
+            <Route path="headerMenu51111bb" component={Hello} />
           </Route>
           <Route path="headerMenu52222">
-            <Route path="headerMenu52222aa" component={Hello}/>
-            <Route path="headerMenu52222bb" component={Hello}/>
+            <Route path="headerMenu52222aa" component={Hello} />
+            <Route path="headerMenu52222bb" component={Hello} />
           </Route>
         </Route>
 
-        <Route path="headerMenu4" component={Hello}/>
-        <Route path="alone" component={Hello}/>
-        <Route path="alone2" component={Hello}/>
+        <Route path="headerMenu4" component={Hello} />
+        <Route path="alone" component={Hello} />
+        <Route path="alone2" component={Hello} />
 
-        <Route path="*" component={Error}/>
-
+        <Route path="*" component={Error} />
       </Route>
     </Router>
   </Provider>
